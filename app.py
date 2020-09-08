2699a01b6d12c9383cda3121028b8629ed3147fe
MagdalenaFilipowicz <magdalenafilipowicznyc@gmail.com>
2020-09-08 23:10:59 +0200

    third add

--- app.py
+++ app.py
@@ -9,12 +9,6 @@ def hello():
     code = get_code()
     print(code)
     return render_template("formu.html", code=code)
-
-@app.route("/calc", methods=["POST"])
-def post_message():
-    x = request.form["amount"]
-    y = 2*int(x)
-    return str(y)
     
 def get_rates():
     response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
@@ -26,5 +20,17 @@ def get_rates():
 def get_code():
     rates = get_rates()
     return [row.get("code") for row in rates]
+
+def get_bid():
+    rates = get_rates()
+    bid_pln = [row.get("bid") for row in rates] 
+    return bid_pln
+
+@app.route("/calc", methods=["POST"])
+def post_message():
+    rates = get_rates()
+    x = request.form["amount"]
+    y = bid_pln*int(x)
+    return str(y)
 
 app.run()
